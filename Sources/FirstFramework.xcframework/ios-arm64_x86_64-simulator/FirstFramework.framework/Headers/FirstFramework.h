34dbb0a9be1249b2c5d590246888404c21d8488f
Tomás <moyanotomasi@gmail.com>
2022-08-25 17:21:06 +0200

    First commit

---
+++ Sources/FirstFramework.xcframework/ios-arm64_x86_64-simulator/FirstFramework.framework/Headers/FirstFramework.h
@@ -0,0 +1,18 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by Tomas Ignacio Moyano on 18.08.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+
